15b5a5a15b684f2c5ade9798d3d55cece5135a68
JustinaXie <32473819+JustinaXie@users.noreply.github.com>
2022-12-03 13:09:52 -0500

    Update Get_Multi_Gene.R

--- R/Get_Multi_Gene.R
+++ R/Get_Multi_Gene.R
@@ -18,13 +18,14 @@ Get_Multi_Gene<-function(output,threshold=0.05){
   tmp_trait1<-lfdr_order_trait1[order(lfdr_order_trait1$index,decreasing = FALSE),]
   tmp_FDR_trait1<-tmp_trait1$FDR_trait1
   Gene_trait1=result$Gene[which(tmp_FDR_trait1<threshold)]
+  result$FDR_trait1<-tmp_FDR_trait1
 
   lfdr_order_trait2<-lfdr[order(lfdr$Z_2,decreasing = FALSE),]
   lfdr_order_trait2$FDR_trait2<-cumsum(lfdr_order_trait2$Z_2)/(1:P)
   tmp_trait2<-lfdr_order_trait2[order(lfdr_order_trait2$index,decreasing = FALSE),]
   tmp_FDR_trait2<-tmp_trait2$FDR_trait2
   Gene_trait2=result$Gene[which(tmp_FDR_trait2<threshold)]
-
+  result$FDR_trait2<-tmp_FDR_trait2
   return(list(Gene_trait1=Gene_trait1,Gene_trait2=Gene_trait2,
               Output=result))
 }
